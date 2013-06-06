9717380b36bec5bcd041e515f0f65ac9c6e875a6
cinabro@physics.wayne.edu <cinabro@cleomac2.physics.wayne.edu>
2013-06-06 11:06:00 -0400

    first commit

---
+++ generate_lsstdfacg.cmd
@@ -0,0 +1,80 @@
+#!/bin/csh
+#
+# David Cinabro, cinabro@physics.wayne.edu
+#
+# Automate the production of a SNANA arbitrary cadence simlib
+# using Lyne Jones' OpsimObs.
+#
+# Directories
+#
+# Where the lsstdfacg fortran executables live
+#
+set lsstdfacgdir = /home/cinabro/LSST
+#
+# where ObsimObs lives
+#
+set opsimobsdir = /home/cinabro/LSST/OpsimObs/OpsimObs-master
+#
+@ NARG = $#argv
+if ( $NARG < 1 ) then
+  echo "Must give the name of the lsstdfacg data file as an argument."
+  echo "I assume it has extension .dat and will produce .SIMLIB"
+  exit
+endif
+#
+# define the data files
+#
+set working = "`pwd`"
+set datastem = $argv[1]
+set datafile = "${datastem}.dat"
+set opsimobsin = "${datastem}_OpsimObs.input"
+set opsimobsout = "${datastem}_OpsimObs.output"
+set simlibin = "${datastem}_OpsimObs_pro.output"
+set simlibfile = "${datastem}.SIMLIB"
+#
+# First pass on the datafile
+#
+echo "First pass with lsstdfacg.out on " $datafile
+#
+cd $lsstdfacgdir
+cp $working/$datafile .
+if (-e $opsimobsin) rm $opsimobsin 
+./lsstdfacg.out << EOD
+I
+$datafile
+$opsimobsin
+EOD
+#
+# Running OpsimObs
+#
+echo "Running OpsimObs on " $opsimobsin
+#
+cp $opsimobsin $opsimobsdir/$opsimobsin
+cd $opsimobsdir
+if (-e $opsimobsout) rm $opsimobsout 
+python opsimObs.py $opsimobsin > $opsimobsout
+cd $lsstdfacgdir
+cp $opsimobsdir/$opsimobsout .
+#
+echo "Second pass with lsstdfacg.out on " $opsimobsout
+#
+if (-e $simlibin) rm $simlibin 
+./lsstdfacg.out << EOD
+C
+$datafile
+$opsimobsout
+$simlibin
+EOD
+#
+echo "Generatiing SIMLIB with lsstdfacg.out on " $simlibin
+#
+if (-e $simlibfile) rm $simlibfile 
+./lsstsimlib.out << EOD
+$simlibin
+$simlibfile
+EOD
+#
+cd $working
+cp $lsstdfacgdir/$simlibfile .
+#
+exit
